d11c3ce656401986cfa27a1b64508e33b957dc62
Austin Wood <i@austinwood.me>
2015-01-25 11:05:30 -0700

    Wrap instance vars in accessor methods

--- lib/merchant_repo.rb
+++ lib/merchant_repo.rb
@@ -1,41 +1,43 @@
 class MerchantRepo
+  attr_reader :merchants
+
   def initialize(merchants)
     @merchants = merchants
   end
 
   def all
-    @merchants
+    merchants
   end
 
   def random
-    @merchants.sample
+    merchants.sample
   end
 
   def find_by_id(id)
-    @merchants.find { |merchant| merchant.id == id }
+    merchants.find { |merchant| merchant.id == id }
   end
 
   def find_by_name(name)
-    @merchants.find { |merchant| merchant.name == name }
+    merchants.find { |merchant| merchant.name == name }
   end
 
   def find_by_time_created(time)
-    @merchants.find { |merchant| merchant.created_at == time }
+    merchants.find { |merchant| merchant.created_at == time }
   end
 
   def find_by_time_updated(time)
-    @merchants.find { |merchant| merchant.updated_at == time }
+    merchants.find { |merchant| merchant.updated_at == time }
   end
 
   def find_all_by_name(name)
-    @merchants.select { |merchant| merchant.name == name }
+    merchants.select { |merchant| merchant.name == name }
   end
 
   def find_all_by_time_created(time)
-    @merchants.select { |merchant| merchant.created_at == time }
+    merchants.select { |merchant| merchant.created_at == time }
   end
 
   def find_all_by_time_updated(time)
-    @merchants.select { |merchant| merchant.updated_at == time }
+    merchants.select { |merchant| merchant.updated_at == time }
   end
 end
