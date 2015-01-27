a5071af27cb3364738b629d865ea77459f2b9ca7
Austin Wood <i@austinwood.me>
2015-01-27 13:12:13 -0700

    Add missing tests

--- test/transaction_repo_test.rb
+++ test/transaction_repo_test.rb
@@ -42,15 +42,6 @@ class TransactionRepositoryTest <  Minitest::Test
     assert_equal transaction2, repo.find_by_result("failed")
   end
 
-  def test_it_finds_all_by_result
-    transaction = Transaction.new(:result => "success")
-    transaction2 = Transaction.new(:result => "failed")
-    transaction3 = Transaction.new(:result => "failed")
-    transactions = [transaction, transaction2, transaction3]
-    repo = TransactionRepo.new(transactions)
-    assert_equal [transaction2, transaction3], repo.find_all_by_result("failed")
-  end
-
   def test_it_finds_transaction_by_time_created
     transaction = Transaction.new(:created_at => "2015-01-23 08:08:45 MST")
     transaction2 = Transaction.new(:created_at => "2015-01-23 08:06:20 MST")
@@ -65,6 +56,33 @@ class TransactionRepositoryTest <  Minitest::Test
     transactions = [transaction, transaction2]
     repo = TransactionRepo.new(transactions)
     assert_equal transaction2, repo.find_by_time_updated("2015-01-23 08:06:20 MST")
+  end
+
+  def test_it_finds_all_by_invoice_id
+    transaction = Transaction.new(:invoice_id => 14)
+    transaction2 = Transaction.new(:invoice_id => 25)
+    transaction3 = Transaction.new(:invoice_id => 25)
+    transactions = [transaction, transaction2, transaction3]
+    repo = TransactionRepo.new(transactions)
+    assert_equal [transaction2, transaction3], repo.find_all_by_invoice_id(25)
+  end
+
+  def test_it_finds_all_by_credit_card_number
+    transaction = Transaction.new(:credit_card_number => 4654405418249632)
+    transaction2 = Transaction.new(:credit_card_number => 4580251236515201)
+    transaction3 = Transaction.new(:credit_card_number => 4580251236515201)
+    transactions = [transaction, transaction2, transaction3]
+    repo = TransactionRepo.new(transactions)
+    assert_equal [transaction2, transaction3], repo.find_all_by_credit_card_number(4580251236515201)
+  end
+
+  def test_it_finds_all_by_result
+    transaction = Transaction.new(:result => "success")
+    transaction2 = Transaction.new(:result => "failed")
+    transaction3 = Transaction.new(:result => "failed")
+    transactions = [transaction, transaction2, transaction3]
+    repo = TransactionRepo.new(transactions)
+    assert_equal [transaction2, transaction3], repo.find_all_by_result("failed")
   end
 
   def test_it_finds_all_transactions_by_time_created
@@ -84,5 +102,4 @@ class TransactionRepositoryTest <  Minitest::Test
     repo = TransactionRepo.new(transactions)
     assert_equal [transaction2, transaction3], repo.find_all_by_time_updated("2015-01-23 08:06:20 MST")
   end
-
 end
