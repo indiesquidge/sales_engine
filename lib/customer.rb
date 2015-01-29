fcf1e42207921b503c43d1d8de9e4ffe4d0f7437
Edward McCarthy <efmccarthy26@gmail.com>
2015-01-29 10:12:11 -0700

    finished business intel for customers

--- lib/customer.rb
+++ lib/customer.rb
@@ -13,4 +13,21 @@ class Customer
     invoice_repo = InvoiceRepo.get_instance
     invoice_repo.find_all_by_customer_id(id)
   end
+
+  def transactions
+    invoices.map { |invoice| invoice.transactions }
+  end
+
+  def successful_invoices
+    invoices.select do |invoice|
+      transaction_results = invoice.transactions
+      transaction_results.select { |transaction| transaction.result == 'success' }
+    end
+  end
+
+  def favorite_merchant
+    merchants = successful_invoices.map { |invoice| invoice.merchant }
+    merchants.max_by { |seller| merchants.count(seller) }
+  end
+
 end
